5a597ffaafa9505c3b13029a9d2c48dc04d1ae22
Glitch (strong-sign) <none>
2018-07-20 09:09:31 +0000

    🐂📉 Checkpoint ./server.js:383348/2877

--- server.js
+++ server.js
@@ -78,6 +78,54 @@ app.post('/api/exercise/add', (req, res) => {
 });
 
 
+app.get('/api/exercise/log', (req, res, next) => {
+  console.log(req.query)
+  if (!req.query.userId) {
+    res.send({error: 'userId must be present'})
+  }
+  
+  const userId = req.query.userId
+  
+  let from = req.query.from;
+  let to = req.query.to;
+  let limit = req.query.limit;
+  
+  const limitOptions = {};
+    if (limit) limitOptions.limit = limit;
+    User.findById(userId)
+      .populate({path: 'log', match: {}, select : '-_id', options: limitOptions})
+      .exec((error, user) => {
+      
+        console.error('Testing invalid userId. After looking for the user, before if(error) and if(user).')
+        console.error('error: ' + error);
+        console.error('user: ' + user);
+      
+        if(error) return res.send({error: error});  
+        if (user){
+          const dataToShow = {id: user._id, username: user.username, count: user.count};
+          if (from) dataToShow.from = from.toDateString();
+          if (to) dataToShow.to = to.toDateString();
+          dataToShow.log = user.log.filter((ej) => {
+            if (from && to) {
+              return ej.date >= from && ej.date <= to;
+            } else if (from) {
+              return ej.date >= from;
+           } else if (to) {
+             return ej.date <= to;
+           } else {
+             return true;
+           }
+          });
+          res.json(dataToShow);
+        } else {
+          res.send({error: 'User not foundd'});
+        }
+    });
+  
+});
+
+
+
 // Not found middleware
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
