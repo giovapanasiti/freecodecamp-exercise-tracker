06ad33d0c97bdd7b898b40e516b860d1ab6484c1
Glitch (strong-sign) <none>
2018-07-20 08:37:01 +0000

    🐪🚂 Checkpoint ./server.js:1500522/107

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/exercise/new-user', (req, res) => {
+  
+});
+
 
 // Not found middleware
 app.use((req, res, next) => {
